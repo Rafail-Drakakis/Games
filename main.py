1ffa3e17c0b2bad27dd6f4b0e92ccf728c4a394e
Rafail-Drakakis <rafaildrakakis123@gmail.com>
2023-10-31 20:14:37 +0200

    added a star catcher game

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ from tic_tac_toe import tic_tac_toe
 from labyrinth import labyrinth
 from Town_of_Salem import game_flow as Salem
 from guess_game import guess_number
+from star_catcher_game import main as star_catcher_game
 
 def main():
-    try:
-        choice = int(input("Press \n1.To play tic tac toe \n2.To play hangman \n3.To play rock paper scissors \n4.To play the pong game \n5.To generate a random labyrinth \n6.To play Town of Salem \n7.To play a guessing game: "))
-        if choice == 1:
-            tic_tac_toe()
-        elif choice == 2:
-            hangman()
-        elif choice == 3:
-            rock_paper_scissors()
-        elif choice == 4:
-            pong_game()
-        elif choice == 5:
-            labyrinth()
-        elif choice == 6:
-            Salem()
-        elif choice == 7:
-            guess_number()
-        else:
-        	print ("Invalid input! ")
-    except ValueError:
-        print("Enter an integer")
-
+    choice = int(input("Press \n1.To play tic tac toe \n2.To play hangman \n3.To play rock paper scissors \n4.To play the pong game \n5.To generate a random labyrinth \n6.To play Town of Salem \n7.To play a guessing game \n8.To play the star catcher game: "))
+    if choice == 1:
+        tic_tac_toe()
+    elif choice == 2:
+        hangman()
+    elif choice == 3:
+        rock_paper_scissors()
+    elif choice == 4:
+        pong_game()
+    elif choice == 5:
+        labyrinth()
+    elif choice == 6:
+        Salem()
+    elif choice == 7:
+        guess_number()
+    elif choice == 8:
+        star_catcher_game()
+    else:
+     	print ("Invalid input! ")
+    
 if __name__ == "__main__":
     main()
